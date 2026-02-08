3e6fe63fc5f24a6a96a897ba00b49e724cf0d8ff
Denis <Oleborn@mail.ru>
2026-02-08 18:58:14 +0700

    5. Добавлены кастомные метрики через AOP и аннотацию

--- src/main/java/oleborn/orderhub_project/order/OrderService.java
+++ src/main/java/oleborn/orderhub_project/order/OrderService.java
@@ -3,6 +3,7 @@ package oleborn.orderhub_project.order;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import oleborn.orderhub_project.order.exception.NotFoundOrderException;
+import oleborn.orderhub_project.order.metrics.annotation.BusinessMetric;
 import oleborn.orderhub_project.order.repository.OrderRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,6 +18,10 @@ public class OrderService {
     private final OrderRepository orderRepository;
 
     @Transactional
+    @BusinessMetric(
+            value = "orders.created",
+            tags = {"operation=create", "type=write"}
+    )
     public Order createOrder(CreateOrderRequest request) {
 
         log.debug("В метод createOrder получен запрос: {}", request);
@@ -39,7 +44,16 @@ public class OrderService {
     }
 
     @Transactional(readOnly = true)
+    @BusinessMetric(
+            value = "orders.retrieved",
+            tags = {"operation=get", "type=read"}
+    )
     public Order getOrderWithItems(Long id) {
+
+        //FIXME пример для бизнес ошибки, потом удалить
+//        if (id == 2){
+//            throw new RuntimeException();
+//        }
 
         log.debug("В метод getOrderWithItems получен запрос поиска order по id: {}", id);
 
